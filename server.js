d86da6ef3c7be4af5688824f8f02f4888afb0833
Andrzej <a.woroniecki88@gmail.com>
2025-01-19 12:15:42 +0100

    main

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const server = http.createServer((req, res) => {
     if (req.method === 'GET' && req.url === '/ping') {
         res.writeHead(200, { 'Content-Type': 'text/plain' });
         res.end('pong');
+        console.log('Ping? Pong!');
     } else {
         res.writeHead(404, { 'Content-Type': 'text/plain' });
         res.end('Not Found');
